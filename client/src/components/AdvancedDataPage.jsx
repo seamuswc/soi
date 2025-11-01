8d7515f4d4210665f9398c2c5f9bb7064792e184
seamus <seamuswconnolly@gmail.com>
2025-11-02 01:45:23 +0900

    Remove Thai text from logout button

--- client/src/components/AdvancedDataPage.jsx
+++ client/src/components/AdvancedDataPage.jsx
@@ -367,7 +367,7 @@ function AdvancedDataPage() {
                 }}
                 className="px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded-lg font-medium transition-colors"
               >
-                Logout / ออกจากระบบ
+                Logout
               </button>
             </div>
           </div>
